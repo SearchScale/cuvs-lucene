40b8676cef34cdcd2e077672a3f35ce85de47824
Ishan Chattopadhyaya <ichattopadhyaya@gmail.com>
2025-08-05 13:37:49 +0530

    Adding the SPI codec

--- hnsw.sh
+++ hnsw.sh
@@ -1,10 +1,18 @@
 rm -rf helloindex *.class
-export LD_LIBRARY_PATH=/home/searchscale/code/cuvs-api/cuvs/cpp/build
-
 mvn -DskipTests=true clean compile package
 
 # Create myindex
 javac -cp "target/test-classes:target/classes:$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q)" NativeHNSWSerialization.java && java -cp  ".:target/test-classes:target/classes:$(mvn dependency:build-classpath   -Dmdep.outputFile=/dev/stdout -q)" NativeHNSWSerialization
 
 # Search on myindex without using GPU
+#javac -cp "target/test-classes:target/classes:$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q)" TestIndexWithLucene101.java && java -cp  ".:target/test-classes:target/classes:$(mvn dependency:build-classpath   -Dmdep.outputFile=/dev/stdout -q)" TestIndexWithLucene101
+
+# Ensure CuVSCodec SPI registration file exists
+mkdir -p src/main/resources/META-INF/services
+echo "com.nvidia.cuvs.lucene.CuVSCodec" > src/main/resources/META-INF/services/org.apache.lucene.codecs.Codec
+
+# Clean and compile the project
+mvn -DskipTests=true clean compile package
+
+# Compile and run TestIndexWithLucene101
 javac -cp "target/test-classes:target/classes:$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q)" TestIndexWithLucene101.java && java -cp  ".:target/test-classes:target/classes:$(mvn dependency:build-classpath   -Dmdep.outputFile=/dev/stdout -q)" TestIndexWithLucene101
