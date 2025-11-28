cb56a4da00cb5c75909cacbff0dde6970748b25c
Vivek Narang <vivek@searchscale.com>
2025-11-27 19:25:22 -0500

    Initial setup

---
+++ benchmarks/src/main/java/com/nvidia/cuvs/lucene/benchmarks/Benchmarks.java
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.nvidia.cuvs.lucene.benchmarks;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class Benchmarks {
+
+  @Benchmark
+  public void testMethod() {}
+}
